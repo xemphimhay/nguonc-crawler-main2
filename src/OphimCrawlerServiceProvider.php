d69e5fbe196970737c70b64f0a19492c8f4a7138
xemphimhay <lanhp871@gmail.com>
2024-05-02 17:50:57 +0700

    Update OphimCrawlerServiceProvider.php

--- src/OphimCrawlerServiceProvider.php
+++ src/OphimCrawlerServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Ophim\Crawler\OphimCrawler;
+namespace nguonc\Crawler\nguoncCrawler;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as SP;
-use Ophim\Crawler\OphimCrawler\Console\CrawlerScheduleCommand;
-use Ophim\Crawler\OphimCrawler\Option;
+use nguonc\Crawler\nguoncCrawler\Console\CrawlerScheduleCommand;
+use nguonc\Crawler\nguoncCrawler\Option;
 
-class OphimCrawlerServiceProvider extends SP
+class nguoncCrawlerServiceProvider extends SP
 {
     /**
      * Get the policies defined on the provider.
@@ -25,29 +25,29 @@ class OphimCrawlerServiceProvider extends SP
         config(['plugins' => array_merge(config('plugins', []), [
             'ggg3/nguon-crawler' =>
             [
-                'name' => 'Ophim Crawler',
+                'name' => 'nguonc Crawler',
                 'package_name' => 'ggg3/nguon-crawler',
                 'icon' => 'la la-hand-grab-o',
                 'entries' => [
-                    ['name' => 'Crawler', 'icon' => 'la la-hand-grab-o', 'url' => backpack_url('/plugin/nguon-crawler')],
-                    ['name' => 'Option', 'icon' => 'la la-cog', 'url' => backpack_url('/plugin/nguon-crawler/options')],
+                    ['name' => 'Crawler', 'icon' => 'la la-hand-grab-o', 'url' => backpack_url('/plugin/nguonc-crawler')],
+                    ['name' => 'Option', 'icon' => 'la la-cog', 'url' => backpack_url('/plugin/nguonc-crawler/options')],
                 ],
             ]
         ])]);
 
         config(['logging.channels' => array_merge(config('logging.channels', []), [
-            'ophim-crawler' => [
+            'nguonc-crawler' => [
                 'driver' => 'daily',
-                'path' => storage_path('logs/hacoidev/ophim-crawler.log'),
+                'path' => storage_path('logs/hacoidev/nguonc-crawler.log'),
                 'level' => env('LOG_LEVEL', 'debug'),
                 'days' => 7,
             ],
         ])]);
 
-        config(['ophim.updaters' => array_merge(config('ophim.updaters', []), [
+        config(['nguonc.updaters' => array_merge(config('nguonc.updaters', []), [
             [
-                'name' => 'Ophim Crawler',
-                'handler' => 'Ophim\Crawler\OphimCrawler\Crawler'
+                'name' => 'nguonc Crawler',
+                'handler' => 'nguonc\Crawler\nguoncCrawler\Crawler'
             ]
         ])]);
     }
@@ -63,12 +63,12 @@ class OphimCrawlerServiceProvider extends SP
         });
 
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'ophim-crawler');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'nguonc-crawler');
     }
 
     protected function loadScheduler()
     {
         $schedule = $this->app->make(Schedule::class);
-        $schedule->command('ophim:plugins:ophim-crawler:schedule')->cron(Option::get('crawler_schedule_cron_config', '*/10 * * * *'))->withoutOverlapping();
+        $schedule->command('nguonc:plugins:nguonc-crawler:schedule')->cron(Option::get('crawler_schedule_cron_config', '*/10 * * * *'))->withoutOverlapping();
     }
 }
